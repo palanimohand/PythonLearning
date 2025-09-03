50900c3d691d3af2904a3477515fc0eb20c1d087
Palanimohan <papmdp.mohan@gmail.com>
2025-09-03 10:11:01 +0530

    Week 1 Home Assignment Submission

--- Basic/V7PolyEncapVariables.py
+++ Basic/V7PolyEncapVariables.py
@@ -1,25 +1,36 @@
-class A:
+# class A:
+#
+#     def __init__(self, a):
+#         print("param constructor",a)
+#
+#     def __init__(self):
+#         print("default constructor")
+#
+#
+#     def method1(self):
+#         print("A method1")
+#
+# class B(A):
+#
+#     def method1(self):
+#         print("B method1")
+#
+#     def method2(self, *args):
+#         print(args)
+#
+# ob = B(10)
+# ob.method1()
+# ob.method2(10)
+# ob.method2("str")
+# ob.method2(10,"str")
 
-    def __init__(self, a):
-        print("param constructor",a)
 
-    def __init__(self):
-        print("default constructor")
+ab = 186
+print(id(ab))
+bc = 186
+print(id(bc))
 
-
-    def method1(self):
-        print("A method1")
-
-class B(A):
-
-    def method1(self):
-        print("B method1")
-
-    def method2(self, *args):
-        print(args)
-
-ob = B(10)
-ob.method1()
-ob.method2(10)
-ob.method2("str")
-ob.method2(10,"str")
+cd = [3,5]
+print(id(cd))
+ef = [3,5]
+print(id(ef))
---
+++ HelloTestLeaf/BreakOut1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+it_experience = int(input("Enter your it experience: "))
+
+if(age>=22 and it_experience>=2):
+    print("Access Granted")
+else:
+    print("Access Denied")
