c0d148b273eec25ce0681037ce3648add0f7f8f7
Palanimohan <papmdp.mohan@gmail.com>
2025-09-12 06:38:12 +0530

    Home Assignment 1 moved to seperate folder

--- HomeAssignments/W1A3.py
+++
@@ -1,3 +0,0 @@
-employee_name = ["Kumar", "Mahesh", "Rajesh", "Alan"]
-for num,name in enumerate(employee_name, start=1):
-    print(num, name)
--- HomeAssignments/W1A4.py
+++
@@ -1,5 +0,0 @@
-amount = int(input("Enter withdrawal Amount : "))
-if amount % 100 ==0:
-    print("Amount Eligible for Withdrawal")
-else:
-    print("Please enter amount in mutliples of 100")
