f5f919df97672f39a7f5f2d3cdb2099ba6f14ae4
Palanimohan <papmdp.mohan@gmail.com>
2025-10-04 21:55:00 +0530

    initiation validation

---
+++ Basic/SequenceInitCheck.py
@@ -0,0 +1,13 @@
+bugs = { }
+discount_set = set()
+number_of_bugs = []
+ganesha = ()
+master = list()
+mummy = tuple()
+million = dict()
+print(type(number_of_bugs))
+print(type(bugs))
+print(type(ganesha))
+print(type(master))
+print(type(mummy))
+print(type(million))
--- HelloTestLeaf/HelloTestLeaf.py
+++ HelloTestLeaf/HelloTestLeaf.py
@@ -1,1 +1,14 @@
 print("Welcome to Test Leaf")
+
+
+def ret():
+    return 1, 2
+
+_, d = ret()
+print(_, d)
+
+import pandas as pd
+
+data = {'Name':['Alice', 'Bob', 'Charlie'], 'Age':[25,30,35]}
+df = pd.DataFrame(data)
+print(df)
