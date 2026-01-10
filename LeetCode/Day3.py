26dda89b60faffbfd535545ce44adc7c0c403320
Palanimohan <papmdp.mohan@gmail.com>
2026-01-10 13:12:09 +0530

    Day 3 In progress of Leet code 30 days of Pandas

---
+++ LeetCode/Day3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = [[1, 'Joe'], [2, 'Henry'], [3, 'Sam'], [4, 'Max']]
+customers = pd.DataFrame(data, columns=['id', 'name']).astype({'id':'Int64', 'name':'object'})
+data = [[1, 3], [2, 1]]
+orders = pd.DataFrame(data, columns=['id', 'customerId']).astype({'id':'Int64', 'customerId':'Int64'})
+print(customers)
+print(customers['id'])
+print(orders['customerId'])
+print(customers['id'][orders['customerId']])
+# customers['id'].loc[] orders['customerId']
